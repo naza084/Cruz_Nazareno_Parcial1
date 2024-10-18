e7ada5f6dcf3e988dd1de2a888c85f1d9aad9365
Cruz Nazareno <cruznazareno05@gmail.com>
2024-10-18 07:15:33 -0300

    Mini base de archivos cargada

---
+++ nombreParcial/src/test/Main.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package test;
+
+/**
+ *
+ * @author PC
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+          // despues del parcial hacer los ejercicios de logica y smell de chat gpt
+    // TENER EN CUENTA LO DE FINAL Y PONER SETS EN EL CONSTRUCTOR DE CHAT GPT, sino de ultima
+    // crear metodos que validen en el set y constructor... me gusta un poco más esa xd
+    // TENER EN CUENTA LO DEL SWITCH DE TRAER MERCANCIA EN SISTEMAMERCANCIA
+    }
+}
